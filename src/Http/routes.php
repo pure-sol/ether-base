49f2c082e24bd1fb0e51e419052475602485586e
Mohammed Anwar <m.anwar@pure-sol.com>
2016-01-11 21:03:04 +0200

    Etherbase structure and base files

--- src/Http/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-Route::group(['namespace' => 'Puresolcom\Etherbase\Http\Controllers'], function() {
-    Route::group(['prefix' => 'backend'], function() {
-        Route::get('home', 'Backend\HomeController@index');
-    });
-});
